a26a4a9a32ad6d19260fd496722b99c66fbfb728
Evelinuxas <159789394+Evelinuxas@users.noreply.github.com>
2024-02-12 16:28:35 +0200

    Create Main.py
    pakeitimai

---
+++ Main.py
@@ -0,0 +1,18 @@
+
+
+
+
+kintamasis=1
+kintamasis2=2
+
+
+def funkcija():
+    print(kintamasis)
+    print(kintamasis2)
+    print(f'test:{kintamasis2}{kintamasis}') #Komentarai
+    print('test:{}{}'.format(kintamasis2, kintamasis))
+
+"""
+       Komentaras
+
+"""
