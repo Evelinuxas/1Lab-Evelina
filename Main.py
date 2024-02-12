9de16706a8eedb21cea1522f17fb14726b090538
Evelinuxas <159789394+Evelinuxas@users.noreply.github.com>
2024-02-12 16:29:53 +0200

    Update Main.py
    Pakeitimas2

--- Main.py
+++ Main.py
@@ -10,7 +10,6 @@ def funkcija():
     print(kintamasis)
     print(kintamasis2)
     print(f'test:{kintamasis2}{kintamasis}') #Komentarai
-    print('test:{}{}'.format(kintamasis2, kintamasis))
 
 """
        Komentaras
